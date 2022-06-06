8a6d35df20565db3462cf4bf5730b9769da76773
Remus Galea <remus.galea@qubiz.com>
2022-06-06 19:08:24 +0300

    Initial Commit

---
+++ todo_new/todo_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_newApp.swift
+//  todo_new
+//
+//  Created by dev on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct todo_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
